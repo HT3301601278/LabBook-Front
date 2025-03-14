a7e2897452c3f2f66d60330e5632d1e55d772450
世间微斯人 <3301601278@qq.com>
2025-03-14 23:54:21 +0800

    完善新增与编辑功能

--- src/views/manager/Lab.vue
+++ src/views/manager/Lab.vue
@@ -45,7 +45,7 @@
     </div>
 
 
-    <el-dialog title="信息" :visible.sync="fromVisible" width="40%" :close-on-click-modal="false" destroy-on-close>
+    <el-dialog title="信息" :visible.sync="fromVisible" width="40%" :close-on-click-modal="false" :append-to-body="true" destroy-on-close>
       <el-form label-width="100px" style="padding-right: 50px" :model="form" :rules="rules" ref="formRef">
         <el-form-item prop="name" label="实验室名称">
           <el-input v-model="form.name" autocomplete="off"></el-input>
