fdefc53dda2d7f51cd9d7f4aeec1851655eb9122
世间微斯人 <3301601278@qq.com>
2025-03-20 22:27:16 +0800

    Update Lab.vue

--- src/views/manager/Lab.vue
+++ src/views/manager/Lab.vue
@@ -20,9 +20,9 @@
         <el-table-column prop="startTime" label="开始时间" show-overflow-tooltip></el-table-column>
         <el-table-column prop="endTime" label="结束时间" show-overflow-tooltip></el-table-column>
         <el-table-column prop="maxReservationHours" label="最大预约时长(小时)" width="140" align="center"></el-table-column>
-        <el-table-column prop="status" label="使用状态" show-overflow-tooltip></el-table-column>
+        <el-table-column prop="usageStatus" label="使用状态" show-overflow-tooltip></el-table-column>
         <el-table-column prop="typeName" label="所属分类" show-overflow-tooltip></el-table-column>
-        <el-table-column prop="labadminName" label="实验室管理员"></el-table-column>
+        <el-table-column prop="labAdminName" label="实验室管理员"></el-table-column>
 
         <el-table-column label="操作" width="180" align="center">
           <template v-slot="scope">
@@ -55,48 +55,51 @@
         <el-form-item prop="labName" label="实验室名称">
           <el-input v-model="form.labName" autocomplete="off"></el-input>
         </el-form-item>
+        <el-form-item prop="startTime" label="开始时间">
           <el-date-picker style="width: 100%"
-              v-model="form.openStartTime"
+              v-model="form.startTime"
               type="datetime"
               value-format="yyyy-MM-dd HH:mm"
               placeholder="请选择开始时间">
           </el-date-picker>
-        <el-form-item prop="startTime" label="开始时间">
         </el-form-item>
+        <el-form-item prop="endTime" label="结束时间">
           <el-date-picker style="width: 100%"
-              v-model="form.openEndTime"
+              v-model="form.endTime"
               type="datetime"
               value-format="yyyy-MM-dd HH:mm"
               placeholder="请选择结束时间">
           </el-date-picker>
-        <el-form-item prop="endTime" label="结束时间">
         </el-form-item>
         <el-form-item prop="maxReservationHours" label="最大预约时长">
-          <el-input-number v-model="form.maxReservationHours" :min="1" :max="24" style="width: 100%"></el-input-number>
+          <el-input-number v-model="form.maxReservationHours" :min="1" :max="24"></el-input-number>
         </el-form-item>
         <el-form-item prop="typeId" label="实验室分类">
           <el-select v-model="form.typeId" placeholder="请选择实验室分类" style="width: 100%">
             <el-option v-for="item in typeData" :key="item.id" :label="item.name" :value="item.id"></el-option>
           </el-select>
         </el-form-item>
-        <el-form-item prop="manual" label="使用手册">
+        <el-form-item prop="labadminId" label="实验室管理员">
+          <el-select v-model="form.labadminId" placeholder="请选择实验室管理员" style="width: 100%">
+            <el-option v-for="item in labadminData" :key="item.id" :label="item.name" :value="item.id"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="使用手册">
           <el-upload
-            class="upload-demo"
-            :action="$baseUrl + '/files/upload'"
-            :on-success="handleManualSuccess"
-            :file-list="manualFileList">
+              class="upload-demo"
+              :action="$baseUrl + '/files/upload'"
+              :on-success="handleManualSuccess"
+              :file-list="manualFileList">
             <el-button size="small" type="primary">点击上传</el-button>
-            <div slot="tip" class="el-upload__tip">只能上传pdf文件</div>
           </el-upload>
         </el-form-item>
-        <el-form-item prop="modelFile" label="3D模型文件">
+        <el-form-item label="3D模型">
           <el-upload
-            class="upload-demo"
-            :action="$baseUrl + '/files/upload'"
-            :on-success="handleModelSuccess"
-            :file-list="modelFileList">
+              class="upload-demo"
+              :action="$baseUrl + '/files/upload'"
+              :on-success="handleModelSuccess"
+              :file-list="modelFileList">
             <el-button size="small" type="primary">点击上传</el-button>
-            <div slot="tip" class="el-upload__tip">只能上传glb文件</div>
           </el-upload>
         </el-form-item>
       </el-form>
@@ -139,6 +142,9 @@ export default {
         typeId: [
           {required: true, message: '请选择实验室分类', trigger: 'blur'},
         ],
+        labadminId: [
+          {required: true, message: '请选择实验室管理员', trigger: 'blur'},
+        ],
         maxReservationHours: [
           {required: true, message: '请设置最大预约时长', trigger: 'blur'},
         ],
@@ -146,18 +152,29 @@ export default {
       ids: [],
       typeData: [],
       manualFileList: [],
-      modelFileList: []
+      modelFileList: [],
+      labadminData: []
     }
   },
   created() {
     this.load(1)
     this.loadType()
+    this.loadLabadmin()
   },
   methods: {
     loadType() {
       this.$request.get('/type/selectAll').then(res => {
         if (res.code === '200') {
           this.typeData = res.data
+        } else {
+          this.$message.error(res.msg)
+        }
+      })
+    },
+    loadLabadmin() {
+      this.$request.get('/labadmin/selectAll').then(res => {
+        if (res.code === '200') {
+          this.labadminData = res.data
         } else {
           this.$message.error(res.msg)
         }
@@ -193,20 +210,12 @@ export default {
       this.fromVisible = true   // 打开弹窗
     },
     handleManualSuccess(response) {
-      if (response.code === '200') {
-        this.form.manual = response.data
-        this.$message.success('使用手册上传成功')
-      } else {
-        this.$message.error(response.msg || '上传失败')
-      }
+      // 把手册文件链接保存到表单中
+      this.form.manual = response.data
     },
     handleModelSuccess(response) {
-      if (response.code === '200') {
-        this.form.modelFile = response.data
-        this.$message.success('3D模型文件上传成功')
-      } else {
-        this.$message.error(response.msg || '上传失败')
-      }
+      // 把3D模型文件链接保存到表单中
+      this.form.modelFile = response.data
     },
     save() {   // 保存按钮触发的逻辑  它会触发新增或者更新
       this.$refs.formRef.validate((valid) => {
